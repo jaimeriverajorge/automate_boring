1205a70bc5601f45ce7d70bf1a98125623084cfd
jaimeriverajorge <70618882+jaimeriverajorge@users.noreply.github.com>
2023-05-01 15:04:49 -0500

    added stringFormat.py

---
+++ pythonBasics/stringFormat.py
@@ -0,0 +1,9 @@
+# This script demonstrates how to format strings
+
+name = "Jorge"
+place = "Chicago"
+time = "8 pm"
+food = "fries"
+
+print('Hello %s, you are invited to a party at %s at %s. Please bring %s.' %
+      (name, place, time, food))
